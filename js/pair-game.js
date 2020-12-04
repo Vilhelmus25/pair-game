85f3df7e5c9c1438b689dafbbb64bf92bffbbcd3
Vilhelmus25 <vorvie@gmail.com>
2020-12-04 22:28:18 +0100

    randomizálás hiányzik, egy apró hiba

--- js/pair-game.js
+++ js/pair-game.js
@@ -1,38 +1,39 @@
 'use strict';
 
 
-const backFaceCardMap = new Map();
-const frontFaceCardMap = new Map();
+//const backFaceCardMap = new Map();
+//const frontFaceCardMap = new Map();
 
 const cards = Array.from(document.querySelectorAll('.card'));
 const backFace = Array.from(document.querySelectorAll('.card__back')).map((elements) => elements);
 const frontFace = Array.from(document.querySelectorAll('.card__front')).map((elements) => elements);
+const ellapsedTimeFace = document.querySelector('.clock');
 
 let matchCounter = 0;
 let flipCounter = 0;
 let firstClick;
 let secondClick;
-
 let ellapsedTime = 0;
 let counterIsRunning = false;
 
-let gameTime = setInterval(() => {
-    if (!counterIsRunning) {
-        return;
-    }
-    ellapsedTime += 1;
-    let ellapsedTimeSeconds = padNumbers(ellapsedTime % 60);
-    let ellapsedTimeMinutes = padNumbers(Math.floor(ellapsedTime / 60));          // a floor lefelé kerekít (floor = padló)
-    const time = `${ellapsedTimeMinutes}:${ellapsedTimeSeconds}`;                 // a másik a Math.ceil()        (ceil mennyezet) fölfelé kerekít
-    const ellapsedTimeFace = document.querySelector('.clock');
-    ellapsedTimeFace.textContent = time;
+const gameTime = () => {
+    setInterval(() => {
+        if (!counterIsRunning) {
+            return;
+        }
+        ellapsedTime += 1;
+        let ellapsedTimeSeconds = padNumbers(ellapsedTime % 60);
+        let ellapsedTimeMinutes = padNumbers(Math.floor(ellapsedTime / 60));          // a floor lefelé kerekít (floor = padló)
+        const time = `${ellapsedTimeMinutes}:${ellapsedTimeSeconds}`;                 // a másik a Math.ceil()        (ceil mennyezet) fölfelé kerekít
 
-}, 1000);
+        ellapsedTimeFace.textContent = time;
 
+    }, 1000);
+}
 // console.log(arrayToMap(backFace, backFaceCardMap));
 // console.log(arrayToMap(frontFace, frontFaceCardMap));
 
-function arrayToMap(arr, faceMap) {                                     // a tömböt mapba teszi
+function arrayToMap(arr, faceMap) {                                                 // a tömböt mapba teszi
     const tempArrayIndex = arr.map((elements, index) => index);
     return faceMap.set(tempArrayIndex, arr);
 }
@@ -57,9 +58,11 @@ function flipCard(card, event) {
     //console.log(event.currentTarget.firstElementChild);
     //let mapIterator = backFaceCardMap.keys();
     counterIsRunning = true;
-    if (flipCounter == 0) {
-        counterIsRunning;
+    flipCounter += 1;
+    if (flipCounter === 1) {
+        gameTime();                             // !!!!!!!!!!!!!!!!!!!!!!!!!!!              sajnos az újraindítás után 2-szeresen megy az óra           !!!!!!!!!!!!!!!!!!
     }
+
     for (let i = 0; i < cards.length; i++) {
         //console.log(backFaceCardMap.values().next().value[i]);
         //console.log(cards[i].childNodes[3].firstElementChild);              // azért childNodes[3], mert az tartalmazza a back__face div-et, de lehet lehet a black__face-el is cards helyett
@@ -67,10 +70,10 @@ function flipCard(card, event) {
             backFace[i].style.transform = "rotateY(-90deg)";
             card.style.transition = "transform 750ms ease-out";
             //console.log(event.currentTarget);
-            console.log(i);
+            // console.log(i);
             firstClick == undefined ? firstClick = i : secondClick = i;
-            flipCounter += 1;
-            i = cards.length;
+
+            i = cards.length + 1;
         }
 
     }
@@ -93,68 +96,57 @@ function checkMatch(first, second) {
         matchCounter += 1;
         firstClick = undefined;
         secondClick = undefined;
-        counterIsRunning = isGameWin(matchCounter);
-        if (!counterIsRunning) {
+        counterIsRunning = isGameWin(matchCounter);                     // itt mindig megnézzük vége van-e a játéknak
+        if (!counterIsRunning) {                                        // ha megáll a számláló
             clearInterval(gameTime);                                    // ez állítja meg az órát
-            gameRestart();
+            gameRestart();                                              // újraindítja a játékot 5 sec után
         }
     } else {
 
 
-        setInterval(() => { // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   Ez forgatja vissza, mert ez egy időzítő és a meghívástól folyamatosan csinálja     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            backFace[first].style.transform = "rotateY(0deg)";
+        setTimeout(() => {                                              // setTimeout az időzítő (sleep)
+            backFace[first].style.transform = "rotateY(0deg)";                  // ez visszaforgatja
             backFace[first].style.transition = "transform 1000ms ease-in";
-        }, 1500);
-
-        backFace[second].style.transform = "rotateY(360deg)";
-        backFace[second].style.transition = "transform 2500ms ease-in";
-
+        }, 2000);                                                               // 2 sec-ig várunk, mielőtt visszaforgatjuk
+        setTimeout(() => {
+            backFace[second].style.transform = "rotateY(0deg)";                 // ez visszaforgatja
+            backFace[second].style.transition = "transform 1000ms ease-in";
+        }, 2000);
 
         firstClick = undefined;
         secondClick = undefined;
     }
 }
 
-// function reFlipCard(card) {
 
-//     if (Array.from(frontFace.map((elements, index) => {
-//         elements == "goblinTarot"
-//     }))) {
-
-//         card.style.transform = "rotateY(90deg)";
-//         card.style.transition = "transform 500ms ease-out";
-//     }
-// }
-
-
-// da clock
 const isGameWin = (matchCounter) => {
-
     return matchCounter == (cards.length / 2) ? false : true;
 };
 
 const padNumbers = (num) => {
-    return num < 10 ? `0${num}` : `${num} `;
+    return num < 10 ? `0${num}` : `${num}`;
 }
 
-// document.querySelector('.card').addEventListener('click', () => {               // ha az órára kattintunk, akkor tudjuk indítani/megállítani
-//     if (counterIsRunning) {
-//         counterIsRunning = false;
-//         ellapsedTime = 0;
-//     } else {
-//         counterIsRunning = true;
-
-//     }
-// })
 
 const gameRestart = () => {
 
-    let delay = setInterval(() => {
+    let delay = setTimeout(() => {
         ellapsedTime = 0;
-
-    }, 5000);
-    if (ellapsedTime == 0) {
         clearInterval(delay);
         ellapsedTimeFace.textContent = `00:00`;
-    }
+        for (let i = 0; i < backFace.length; i++) {                         // ez visszaállítja az eredeti állapotába a játékot
+            backFace[i].style.transform = "rotateY(0deg)";
+            backFace[i].style.transition = "transform 1000ms ease-in";
+        }
+    }, 5000);
+
+    matchCounter = 0;
+    flipCounter = 0;
+    firstClick = null;
+    secondClick = null;
+
+    ellapsedTime = 0;
+    counterIsRunning = false;
+
+
 }
