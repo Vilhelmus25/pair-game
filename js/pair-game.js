f8c0ef14c2a13e98e68501f84bdf641529b4ac95
Vörös Vilmos (Admin) <admin_vorosv@uni-bge.hu>
2020-12-04 12:48:28 +0100

    van játék vége, nincs restart,random, kisebb hibák

--- js/pair-game.js
+++ js/pair-game.js
@@ -8,12 +8,11 @@ const cards = Array.from(document.querySelectorAll('.card'));
 const backFace = Array.from(document.querySelectorAll('.card__back')).map((elements) => elements);
 const frontFace = Array.from(document.querySelectorAll('.card__front')).map((elements) => elements);
 
+let matchCounter = 0;
 let flipCounter = 0;
 let firstClick;
 let secondClick;
-const cardPositionStore = {
-    arr: [0, 0]
-};
+
 
 console.log(arrayToMap(backFace, backFaceCardMap));
 console.log(arrayToMap(frontFace, frontFaceCardMap));
@@ -42,13 +41,16 @@ backFace.forEach((card) => {
 function flipCard(card, event) {
     //console.log(event.currentTarget.firstElementChild);
     //let mapIterator = backFaceCardMap.keys();
-
+    counterIsRunning = true;
+    if (flipCounter == 0) {
+        counterIsRunning;
+    }
     for (let i = 0; i < cards.length; i++) {
         //console.log(backFaceCardMap.values().next().value[i]);
         //console.log(cards[i].childNodes[3].firstElementChild);              // azért childNodes[3], mert az tartalmazza a back__face div-et, de lehet lehet a black__face-el is cards helyett
         if (cards[i].childNodes[3].firstElementChild == event.currentTarget.firstElementChild) {
             backFace[i].style.transform = "rotateY(-90deg)";
-            card.style.transition = "transform 500ms ease-out";
+            card.style.transition = "transform 750ms ease-out";
             //console.log(event.currentTarget);
             console.log(i);
             firstClick == undefined ? firstClick = i : secondClick = i;
@@ -65,28 +67,33 @@ function checkMatch(first, second) {
     const firstCard = cards[first].childNodes[1].firstElementChild.className;               // jaj de nehéz szülés volt így beazonosítani!!! A konzol sokat segített.
     const secondCard = cards[second].childNodes[1].firstElementChild.className;             // a kattintott kártya childNodejai a divek és a textContentek, a [1]-es a frontface div. Annak az első gyereke (node-ja a firstElementChild adja vissza). Annak a node-nak van egy className propertyje, azok egyedik
     const checkCards = {
-        firstCard,
+        firstCard,                      //  property-shorthand!!!
         secondCard,
-        // getFirstCard: function () {
-        //     return this.firstCard;
-        // },
-        // getSecondCard: function () {
-        //     return this.secondCard;
-        // }
     }
     console.log(Object.values(checkCards)[0]);
     console.log(Object.values(checkCards)[1]);
 
 
     if (Object.values(checkCards)[0] == Object.values(checkCards)[1]) {
-
+        matchCounter += 1;
         firstClick = undefined;
         secondClick = undefined;
+        counterIsRunning = isGameWin(matchCounter);
+        if (!counterIsRunning) {
+            clearInterval(gameTime);                                    // ez állítja meg az órát
+            gameRestart();
+        }
     } else {
-        backFace[first].style.transform = "rotateY(0deg)";
+
+
+        setInterval(() => {
+            backFace[first].style.transform = "rotateY(0deg)";
+            backFace[first].style.transition = "transform 1000ms ease-in";
+        }, 1500);
+
         backFace[second].style.transform = "rotateY(360deg)";
-        backFace[first].style.transition = "transform 1000ms ease-in";
-        backFace[second].style.transition = "transform 1500ms ease-in";
+        backFace[second].style.transition = "transform 2500ms ease-in";
+
 
         firstClick = undefined;
         secondClick = undefined;
@@ -106,15 +113,29 @@ function checkMatch(first, second) {
 
 
 // da clock
+const isGameWin = (matchCounter) => {
 
+    return matchCounter == (cards.length / 2) ? false : true;
+};
+const gameRestart = () => {
 
+    let delay = setInterval(() => {
+        ellapsedTime = 0;
+
+    }, 5000);
+    if (ellapsedTime == 0) {
+        clearInterval(delay);
+        ellapsedTimeFace.textContent = `00:00`;
+    }
+}
 const padNumbers = (num) => {
     return num < 10 ? `0${num}` : `${num} `;
 }
 
 let ellapsedTime = 0;
-let counterIsRunning = true;
-setInterval(() => {
+let counterIsRunning = false;
+
+let gameTime = setInterval(() => {
     if (!counterIsRunning) {
         return;
     }
@@ -124,14 +145,18 @@ setInterval(() => {
     const time = `${ellapsedTimeMinutes}:${ellapsedTimeSeconds}`;                 // a másik a Math.ceil()        (ceil mennyezet) fölfelé kerekít
     const ellapsedTimeFace = document.querySelector('.clock');
     ellapsedTimeFace.textContent = time;
+
 }, 1000);
 
-document.querySelector('.clock').addEventListener('click', () => {
-    if (counterIsRunning) {
-        counterIsRunning = false;
-        ellapsedTime = 0;
-    } else {
-        counterIsRunning = true;
 
-    }
-})
+
+
+// document.querySelector('.card').addEventListener('click', () => {               // ha az órára kattintunk, akkor tudjuk indítani/megállítani
+//     if (counterIsRunning) {
+//         counterIsRunning = false;
+//         ellapsedTime = 0;
+//     } else {
+//         counterIsRunning = true;
+
+//     }
+// })
